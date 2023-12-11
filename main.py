15ef30d4548fd8dacce6d8f85d2c35b2dece4037
wolflergf <wolflergf@gmail.com>
2023-12-11 21:57:14 +0000

    Add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import requests
 from send_email import send_email
+from dotenv import load_dotenv
+import os
 
-api_key = ""
-url = "https://newsapi.org/v2/everything?q=python&from=2023-11-11&sortBy=publishedAt&apiKey="
-request = requests.get(url)
-content = request.json()
-# print(content["articles"])
+load_dotenv()
+secret_key = os.getenv("SECRET_KEY")
+
 
 def get_message():
-    message = ""
-    for article in content["articles"]:        
+    NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+    NEWS_API_KEY = secret_key
+    KEY_WORD = "python, -swallows, -swallowed"
+    LANGUAGE = "en"
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": KEY_WORD,
+        "language": LANGUAGE,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    twenty_articles = articles[:20]
+    subject = "Subject: Top 20 news about Python\n"
+    message = "{}".format(subject)
+    for article in twenty_articles:
         message += article["title"] + "\n" + article["url"] + "\n\n"
     return message
+
+
 
 send_email(get_message().encode("utf-8"))
--- send_email.py
+++ send_email.py
@@ -1,13 +1,20 @@
 import smtplib, ssl
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+my_email = os.getenv("MY_EMAIL")
+password_email = os.getenv("API_GOOGLE")
+
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "@gmail.com"
-    password = ""
+    username = my_email
+    password = password_email
 
-    receiver = "@gmail.com"
+    receiver = my_email
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
